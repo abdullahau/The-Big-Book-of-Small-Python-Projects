9a0599abd9a3c10398811835a2ffb1cab6934b17
abdullahmahmood <abdullah.au@outlook.com>
2024-07-29 16:13:50 +0400

    update

--- Projects/Project 1/bagels.py
+++ Projects/Project 1/bagels.py
@@ -1,8 +1,12 @@
 import random
 
-print('''Bagels, a deductive logic game.
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+  print('''Bagels, a deductive logic game.
 By Al Sweigart al@inventwithpython.com
-  
+
 I am thinking of a 3-digit number with no repeated digits.
 Try to guess what it is. Here are some clues:
 When I say:    That means:
@@ -13,3 +17,54 @@ When I say:    That means:
 For example, if the secret number was 248 and your guess was 843, the
 clues would be Fermi Pico.''')
 
+  while True:
+    secretNum = getSecretNum()
+    print('I have thought up a number.')
+    print(f' You have {MAX_GUESSES} guesses to get it.')
+    
+    numGuesses = 1
+    
+    while numGuesses <= MAX_GUESSES:
+      print(f'Guess #{numGuesses}')
+      guess = input('> ')
+      
+      clues = getClues(guess, secretNum)
+      print(clues)
+      numGuesses += 1
+      
+      if guess == secretNum:
+        break
+      if numGuesses > MAX_GUESSES:
+        print('You ran out of guesses.')
+        print(f'The answer was {secretNum}.')
+        
+    print('Do you want to play again? (yes or no)')
+    if not input('> ').lower().startswith('y'):
+      break
+  print('Thanks for playing!')
+    
+def getSecretNum():
+  numbers = list('0123456789')
+  random.shuffle(numbers)
+  return "".join(numbers[0:NUM_DIGITS])
+
+def getClues(guess, secretNum):
+  if guess == secretNum:
+    return 'You got it!'
+  
+  clues = []
+  
+  for i in range(NUM_DIGITS):
+    if guess[i] == secretNum[i]:
+      clues.append("Fermi")
+    elif guess[i] in secretNum:
+      clues.append("Pico")
+  
+  if len(clues) == 0:
+    return 'Bagels'
+  else:
+    clues.sort()
+    return ' '.join(clues)
+  
+if __name__ == "__main__":
+  main()
